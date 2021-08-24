4b1517e68fe5ac16f3b61e6190832aa3e6ac1fcd
tarushi-k <arrow0905@gmail.com>
2021-08-24 23:22:26 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
 
 
